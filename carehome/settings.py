9beb5d076ab199e9960fec3142988e0067046052
AkhinaLalu <akhinalalu10@gmail.com>
2025-03-10 14:23:31 +0530

    updates

--- carehome/settings.py
+++ carehome/settings.py
@@ -146,9 +146,8 @@ MEDIA_URL = '/media/'
 #STATIC_ROOT = os.path.join(BASE_DIR, "static")
 #MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'admin_template', 'assets')
+    os.path.join(BASE_DIR, 'static'), 
 ]
-
 
 
 
