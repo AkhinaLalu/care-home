bcc168d5e9f8a31f36a3fcdbb741d64ce82d04d2
AkhinaLalu <akhinalalu10@gmail.com>
2025-03-10 14:21:55 +0530

    updates

--- carehome/settings.py
+++ carehome/settings.py
@@ -146,8 +146,11 @@ MEDIA_URL = '/media/'
 #STATIC_ROOT = os.path.join(BASE_DIR, "static")
 #MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'), 
+    os.path.join(BASE_DIR, 'admin_template', 'assets')
 ]
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
